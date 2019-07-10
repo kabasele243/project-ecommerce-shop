628049993b8814b3a0a5d8618ff89a3e2beb9f04
Kabasele243 <fkabasele7@gmail.com>
2019-07-10 14:31:23 -0400

    Adding shop page, collection preview and collection item

---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import CollectionItem from "../collection-item/collection-item.components";
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemsProps }) => (
+          <CollectionItem key={id} {...otherItemsProps} />
+        ))}
+    </div>
+  </div>
+);
+export default CollectionPreview;
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionsProps }) => (
+          <CollectionPreview keys={id} {...otherCollectionsProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
