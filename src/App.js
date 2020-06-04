a83d82ada99de17a9ae8f436d1a4aaa3fef63458
Franck Kabasele <kabasele467@gmail.com>
2020-06-04 10:41:53 -0400

    fixing button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
@@ -40,6 +41,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -18,7 +18,7 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton className="collection-item-button" onClick={() => addItem(item)} inverted>
         Add to cart
       </CustomButton>
     </div>
